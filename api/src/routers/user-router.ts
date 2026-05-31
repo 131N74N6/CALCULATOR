e2924eb629bdbbea4a1a8a3406b6fccd04207cfd
unknown <bintangkusumam@gmail.com>
2026-05-31 08:22:09 +0700

    >_<

--- api/src/routers/user-router.ts
+++ api/src/routers/user-router.ts
@@ -5,7 +5,7 @@ import { checkOwnerShip, verifyToken } from "../middleware/auth-middleware";
 const userRoutes = Router();
 
 userRoutes.delete('/rm/:user_id', verifyToken, checkOwnerShip, deleteCurrentUser);
-userRoutes.get('/user-data', verifyToken, getCurrentUserData);
+userRoutes.get('/user-data', getCurrentUserData);
 userRoutes.put('/edit/:user_id', verifyToken, checkOwnerShip, changeCurrentUserData);
 
 export default userRoutes;
